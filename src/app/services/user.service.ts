a30880efb80e6b76bb97e4a2d51d0ed8a19d5113
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2020-01-01 20:29:48 +0100

    refactor

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Observable, throwError } from "rxjs";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
 
 @Injectable({
